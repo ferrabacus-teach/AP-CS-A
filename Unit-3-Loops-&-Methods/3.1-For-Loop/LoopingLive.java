3038cd4c23f148424f3b80cf2d1d533778d1599b
ferrabacus <ferrabacus@gmail.com>
2018-10-19 11:46:22 -0700

    Added 3.1 Live

---
+++ Unit-3-Loops-&-Methods/3.1-For-Loop/LoopingLive.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LoopingLive {
+  public static void main(String[] args) {
+
+  Scanner scan = new Scanner(System.in);
+
+  // for(int i = 5; i < 15; i++) {
+  //   System.out.println(i + " Mr. Ferra is awesome!");
+  // }
+
+  // System.out.print("Can you give me a number: ");
+  // int oi = scan.nextInt();
+  //
+  // for(int i = 0; i < oi; i++) {
+  //   System.out.println(i + "Ferrabacus Rules!");
+  // }
+  // }
+
+  for(char a = 0; a<=100000; a++){
+    System.out.print(a);
+  }
+}
+}
