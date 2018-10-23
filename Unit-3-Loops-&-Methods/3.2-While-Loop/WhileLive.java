be3a4c3c32c3d712467ac1e56a2b12cc1b8fa46d
ferrabacus <ferrabacus@gmail.com>
2018-10-23 12:05:06 -0700

    added 3.2 live

---
+++ Unit-3-Loops-&-Methods/3.2-While-Loop/WhileLive.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class WhileLive {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    // While loop that runs 10 times.
+    // Print the iteration number each time.
+
+    // int numb = 0;
+    //
+    // while(numb <= 10) {
+    //   System.out.println(numb);
+    //   numb++;
+    // }
+
+    // Write a while loop.
+    // That runs until they say Jared.
+    // Inside the loop, keep asking for a word.
+
+    // String chief = "";
+    // while(!chief.equals("Jared")) {
+    //   System.out.println("Enter a word: ");
+    //   chief = scan.next();
+    // }
+
+    // break
+    while(true) {
+      System.out.println("Infinite Loop? ");
+
+      continue;
+
+
+    }
+
+    // continue
+
+
+
+
+
+
+
+
+
+    // String answer = "";
+    // while(!answer.equals("exit")) {
+    //     // will this run?
+    //     System.out.println("This will run until you say exit");
+    //     System.out.println("Enter words: ");
+    //     answer = scan.next();
+    //
+    //     System.out.println("Here is your word: " + answer);
+    //
+    //     System.out.println(answer.equals("exit"));
+    //
+    //     System.out.println(!answer.equals("exit"));
+    // }
+
+    // int countJared = 0;
+    // int countCheeseCake = 0;
+    // while(true) {
+    //   System.out.println("Enter words: ");
+    //   String word = scan.next();
+    //
+    //   if(word.equals("Jared")) {
+    //     countJared++;
+    //   } else if(word.equals("CheeseCake")){
+    //     countCheeseCake++;
+    //   }
+    //   System.out.println("Jareds" +countJared);
+    //   System.out.println("Cheese Cakes" +countCheeseCake);
+    //   if(word.equals("exit")) {
+    //     break;
+    //   }
+    // }
+
+
+
+  }
+}
